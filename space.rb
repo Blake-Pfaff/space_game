4891219539fa6599ced31884de39cafff1c68a42
Blake Pfaff <blake.a.pfaff2@gmail.com>
2018-03-15 21:06:38 -0500

    adds window for images

---
+++ space.rb
@@ -0,0 +1,18 @@
+require 'gosu'
+
+class Tutorial < Gosu::Window
+  def initialize
+    super 640, 480
+    self.caption = "Dat_Ruby_Space_Game"
+  end
+
+  def update
+    # ...
+  end
+
+  def draw
+    # ...
+  end
+end
+
+Tutorial.new.show
